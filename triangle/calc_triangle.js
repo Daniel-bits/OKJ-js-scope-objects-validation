4f5a6bf5cbfbf51532f6bf95318fabd1b534e27c
Daniel-bits <d.dobrentey@protonmail.com>
2022-02-12 22:09:08 +0100

    Feladat 3

---
+++ triangle/calc_triangle.js
@@ -0,0 +1,34 @@
+﻿function kiszamit() {
+	const aOldal = document.getElementById('A');
+	const bOldal = document.getElementById('B');
+	const cOldal = document.getElementById('C');
+	const a = parseFloat(aOldal.value);
+	const b = parseFloat(bOldal.value);
+	const c = parseFloat(cOldal.value);
+
+	validate(a, b, c);
+
+	let szoveg = "";
+
+	const K = a + b + c;
+	const s = K / 2;
+	const T = Math.sqrt(s * (s - a)*(s - b)*(s - c));
+
+	szoveg += "Kerület: " + K.toFixed(2) + "<br>";
+	szoveg += "Terület: " + T.toFixed(2);
+
+	const kimenet = document.querySelector('#output span');
+	kimenet.innerHTML = szoveg + " hosszúságegység";
+
+}
+
+let validate = (a, b, c) => {
+	if (!a || !b || !c || Number.isNaN(a) || Number.isNaN(b) || Number.isNaN(c)) {
+		alert("Baromságokat írt be");
+		return;
+	}
+	if ((a + b <= c) || (b + c <= a) || (c + a <= b)) {
+		alert("Nem háromszöget adott meg");
+		return;
+	}
+}
